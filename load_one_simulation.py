a674fccdd5525bc33658e4d7d16c51e6acebd5dd
EveCharbie <eve.charbonneau.1@umontreal.ca>
2022-02-01 11:10:47 -0500

    added possibility to visualize for 15DoFs

--- load_one_simulation.py
+++ load_one_simulation.py
@@ -2,17 +2,26 @@
 import bioviz
 import numpy as np
 from bioptim import OptimalControlProgram
+import pickle
 
-model_name = "Model_JeCh_10DoFs.bioMod"
-biorbd_viz = bioviz.Viz(model_name)
-file = "Model_JeCh_10DoFs.bo"
-# Loading
-ocp, sol = OptimalControlProgram.load(file)
+nb_DoFs = 15
+if nb_DoFs == 10:
+    model_name = "Model_JeCh_10DoFs.bioMod"
+    file = "Model_JeCh_10DoFs.bo"
+    # Loading
+    ocp, sol = OptimalControlProgram.load(file)
+    q = sol.states["q"]
 
-q = sol.states["q"]
+elif nb_DoFs == 15:
+    file_name = ".... .pckl"
+    model_name = "Model_JeCh_15DoFs.bioMod"
+    file = open(f"{file_name}", "rb")
+    data = pickle.load(file)
+    q = np.hstack((data["states"][0]['q'], data["states"][1]['q']))
 
-# Animate the mode
+# Animate the model
 manually_animate = False
+biorbd_viz = bioviz.Viz(model_name)
 if manually_animate:
     i = 0
     while biorbd_viz.vtk_window.is_active:
@@ -21,3 +30,4 @@ if manually_animate:
 else:
     biorbd_viz.load_movement(q)
     biorbd_viz.exec()
+
