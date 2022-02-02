2c4204ea5d266e7d11fb4b34e1b461631dabc2f6
Ipuch <puchaud.pierre@gmail.com>
2022-02-02 14:30:29 -0500

    loading simulation

--- load_one_simulation.py
+++ load_one_simulation.py
@@ -2,12 +2,14 @@ import bioviz
 import numpy as np
 from bioptim import OptimalControlProgram
 
-model_name = "Model_JeCh_10DoFs.bioMod"
-biorbd_viz = bioviz.Viz(model_name)
-file = "Model_JeCh_10DoFs.bo"
-# Loading
-ocp, sol = OptimalControlProgram.load(file)
+nb_DoFs = 15
 
+if nb_DoFs == 10:
+    model_name = "Model_JeCh_10DoFs.bioMod"
+    file = "Model_JeCh_10DoFs.bo"
+    # Loading
+    ocp, sol = OptimalControlProgram.load(file)
+    q = sol.states["q"]
 elif nb_DoFs == 15:
     # file_name = "/home/user/Documents/Programmation/Eve/Tests_NoteTech_Pierre/results/miller_explicit_irand/miller_explicit_irand1.pckl"
     file_name = "/home/puchaud/Projets_Python/OnDynamicsForSommersaults_results/other/miller_root_explicit_irand0.pckl"
@@ -15,7 +17,6 @@ elif nb_DoFs == 15:
     file = open(f"{file_name}", "rb")
     data = pickle.load(file)
     q = np.hstack((data["states"][0]['q'], data["states"][1]['q']))
-q = sol.states["q"]
 
 # Animate the mode
 manually_animate = False
