264c6853b8eb8b1fe93b19df2fc4edb20453a79c
Ipuch <puchaud.pierre@gmail.com>
2022-02-10 16:11:40 -0500

    miller viz

--- miller_viz.py
+++ miller_viz.py
@@ -1,10 +1,16 @@
 import numpy as np
 import biorbd_casadi as biorbd
+from bioptim import PlotType, BiorbdInterface
 
 
 def plot_linear_momentum(x, nlp):
-    linear_momentum_func = nlp.model.mass() * biorbd.to_casadi_func("LinearMomentum",  nlp.model.CoMdot,
-                                                 nlp.states["q"].mx, nlp.states["qdot"].mx, expand=False)
+    linear_momentum = (
+        nlp.model.mass().to_mx() * nlp.model.CoMdot(nlp.states["q"].mx, nlp.states["qdot"].mx, True).to_mx()
+    )
+
+    linear_momentum_func = biorbd.to_casadi_func(
+        "LinearMomentum", linear_momentum, nlp.states["q"].mx, nlp.states["qdot"].mx, expand=False
+    )
 
     q = nlp.states["q"].mapping.to_second.map(x[nlp.states["q"].index, :])
     qdot = nlp.states["qdot"].mapping.to_second.map(x[nlp.states["qdot"].index, :])
@@ -13,8 +19,9 @@ def plot_linear_momentum(x, nlp):
 
 
 def plot_angular_momentum(x, nlp):
-    angular_momentum_func = biorbd.to_casadi_func("AngularMomentum", nlp.model.angularMomentum,
-                                                  nlp.states["q"].mx, nlp.states["qdot"].mx, expand=False)
+    angular_momentum_func = biorbd.to_casadi_func(
+        "AngularMomentum", nlp.model.angularMomentum, nlp.states["q"].mx, nlp.states["qdot"].mx, expand=False
+    )
 
     q = nlp.states["q"].mapping.to_second.map(x[nlp.states["q"].index, :])
     qdot = nlp.states["qdot"].mapping.to_second.map(x[nlp.states["qdot"].index, :])
@@ -22,15 +29,21 @@ def plot_angular_momentum(x, nlp):
     return np.array(angular_momentum_func(q, qdot))
 
 
-def plot_residual_torque(x, nlp):
-    ID_func = biorbd.to_casadi_func("ResidualTorque", nlp.model.InverseDynamics,
-                                                  nlp.states["q"].mx, nlp.states["qdot"].mx, nlp.controls["qddot"].mx, expand=True)
+def plot_residual_torque(x, u, nlp):
+    ID_func = biorbd.to_casadi_func(
+        "ResidualTorque",
+        nlp.model.InverseDynamics,
+        nlp.states["q"].mx,
+        nlp.states["qdot"].mx,
+        nlp.controls["qddot"].mx,
+        expand=True,
+    )
 
     q = nlp.states["q"].mapping.to_second.map(x[nlp.states["q"].index, :])
     qdot = nlp.states["qdot"].mapping.to_second.map(x[nlp.states["qdot"].index, :])
-    qddot = nlp.controls["qddot"]
+    qddot = u[nlp.controls["qddot"].index, :]
 
-    return np.array(ID_func(q, qdot, qddot)[:6])
+    return np.array(ID_func(q, qdot, qddot)[:6, :])
 
 
 def add_custom_plots(ocp, dynamics_type):
@@ -39,12 +52,34 @@ def add_custom_plots(ocp, dynamics_type):
         #     "LinearMomentum", lambda t, x, u, p: plot_linear_momentum(x, nlp), phase=i,
         #     legend=["Linear Momentum x", "Linear Momentum y", "Linear Momentum z"]
         # )
+
         ocp.add_plot(
-            "AngularMomentum", lambda t, x, u, p: plot_angular_momentum(x, nlp), phase=i,
-            legend=["Angular Momentum x", "Angular Momentum y", "Angular Momentum z"]
+            "LinearMomentum",
+            lambda t, x, u, p: plot_linear_momentum(x, nlp),
+            phase=i,
+            legend=["Linear Momentum x", "Linear Momentum y", "Linear Momentum z"],
+            plot_type=PlotType.PLOT,
+        )
+
+        ocp.add_plot(
+            "AngularMomentum",
+            lambda t, x, u, p: plot_angular_momentum(x, nlp),
+            phase=i,
+            legend=["Angular Momentum x", "Angular Momentum y", "Angular Momentum z"],
+            plot_type=PlotType.PLOT,
         )
         if "implicit" in dynamics_type:
             ocp.add_plot(
-                "AngularMomentum", lambda t, x, u, p: plot_residual_torque(x, nlp), phase=i,
-                legend=["Angular Momentum x", "Angular Momentum y", "Angular Momentum z"]
+                "TorqueResiduals",
+                lambda t, x, u, p: plot_residual_torque(x, u, nlp),
+                phase=i,
+                legend=[
+                    "TorqueResiduals x",
+                    "TorqueResiduals y",
+                    "TorqueResiduals z",
+                    "TorqueResiduals Rx",
+                    "TorqueResiduals Ry",
+                    "TorqueResiduals Rz",
+                ],
+                plot_type=PlotType.INTEGRATED,
             )
