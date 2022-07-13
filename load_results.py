0cc010378c33e7aedfef7c27279a5a796f4caa4a
Ipuch <puchaud.pierre@gmail.com>
2022-07-13 14:01:59 -0400

    load result better

--- load_results.py
+++ load_results.py
@@ -8,22 +8,28 @@ from bioptim import OptimalControlProgram
 # filename = "raw/miller_MillerDynamics.EXPLICIT_irand10_extraobjFalse_125_25.pckl"  # or .bo, any file in raw.
 filename = "raw/miller_MillerDynamics.ROOT_IMPLICIT_irand0_extraobjTrue_125_25.bo"
 
-if filename.endswith(".bo"):
-    ocp, sol = OptimalControlProgram.load(filename)
-    sol.print_cost()
-    sol.animate(n_frames=100)
+def show_animation(model_path: str, q: np.ndarray, fixed_floating_base: bool = True):
+    """
+    Show the animation of the model
 
-elif filename.endswith(".pckl"):
-    model_name = "Model_JeCh_15DoFs.bioMod"
-    file = open(f"{filename}", "rb")
-    data = pickle.load(file)
-    q = np.hstack((data["states"][0]["q"], data["states"][1]["q"]))
+    Parameters
+    ----------
+    model_path : str
+        Model to animate
+    q : np.ndarray
+        Array of the states to animate
 
-    # Animate the model
-    manually_animate = False
+    Returns
+    -------
+    None
+    """
     import bioviz
+    manually_animate = False
+    biorbd_viz = bioviz.Viz(model_path, show_floor=False, show_gravity_vector=False)
 
-    biorbd_viz = bioviz.Viz(model_name, show_floor=False, show_gravity_vector=False)
+    if fixed_floating_base:
+        q[:6, :] = 0
+
     if manually_animate:
         i = 0
         while biorbd_viz.vtk_window.is_active:
@@ -32,5 +38,27 @@ elif filename.endswith(".pckl"):
     else:
         biorbd_viz.load_movement(q)
         biorbd_viz.exec()
+
+
+if filename.endswith(".bo"):
+    try:
+        ocp, sol = OptimalControlProgram.load(filename)
+        sol.print_cost()
+        sol.animate(n_frames=100)
+    except:
+        model_name = "Model_JeCh_15DoFs.bioMod"
+        file = open(f"{filename}", "rb")
+        data = pickle.load(file)
+        q = np.hstack((data[0][0]["q"], data[0][1]["q"]))
+        show_animation(model_name, q)
+
+elif filename.endswith(".pckl"):
+    model_name = "Model_JeCh_15DoFs.bioMod"
+    file = open(f"{filename}", "rb")
+    data = pickle.load(file)
+    q = np.hstack((data["states"][0]["q"], data["states"][1]["q"]))
+
+    # Animate the model
+    show_animation(model_name, q)
 else:
     raise ValueError("filename must end with .bo or .pkl")
